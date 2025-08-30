ed61bf962659a6eb848121d6d650b6b72fa09aad
FadeIT <wagener.90@gmx.de>
2025-08-30 02:00:13 +0200

    implemented JoinListener

--- src/main/java/de/fade/hideAndSeek/events/OnPlayerJoinEvent.java
+++ src/main/java/de/fade/hideAndSeek/events/OnPlayerJoinEvent.java
@@ -1,6 +1,7 @@
 package de.fade.hideAndSeek.events;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.title.Title;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -12,11 +13,11 @@ public class OnPlayerJoinEvent implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-
-        Title title = Title.title(Component.text("test").,Component.text("test"));
-
         Player player = event.getPlayer();
+
+        Title title = Title.title(Component.text(player.getName() + " Welcome to").color(TextColor.color(0, 215, 255)),Component.text("Hide And Seek!").color(TextColor.color(252, 255, 80)));
         player.playSound(player, Sound.ENTITY_PLAYER_LEVELUP, 1.0F, 1.0F);
         player.showTitle(title);
+        event.joinMessage(Component.text("§7[§a+§7] §f" + player.getName() + " joined!"));
     }
 }
